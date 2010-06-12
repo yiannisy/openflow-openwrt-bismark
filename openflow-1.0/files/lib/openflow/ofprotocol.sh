7b14513922101915b4a928af56e223db5bb53281
= <=>
2010-06-11 21:44:53 -0700

    Clean up, removing old files and configurations.

--- openflow-1.0/files/lib/openflow/ofprotocol.sh
+++ openflow-1.0/files/lib/openflow/ofprotocol.sh
@@ -2,7 +2,6 @@
 # Copyright (C) 2009 OpenFlowSwitch.org
 
 setup_ofprotocol() {
-#	local iface="$1"
 	local config="$1"
 	local ofctl
 	local dp
@@ -17,12 +16,10 @@ setup_ofprotocol() {
 
 	pid="$(cat "$pidfile" 2>/dev/null)"
 
-	echo "My controller is $ofctl and datapath is $dp"
-
 	if [ -d "/proc/$pid" ] && grep ofprotocol "/proc/${pid}/cmdline" >/dev/null 2>/dev/null; then
 		lock -u "/var/lock/ofprotocol"
 	else
-		[ -z "$dp" -o -z "$ofctl" ] && echo "no datapath or controller specified" && return 1
+		[ -z "$dp" -o -z "$ofctl" ] && echo "no controller specified" && return 1
 		ofprotocol unix:/var/run/"$dp".sock "$ofctl" --fail=closed "-D" "--pidfile=$pidfile" &
 		lock -u "/var/lock/ofprotocol"
 	fi
@@ -32,18 +29,16 @@ setup_inband_control() {
 	local config="$1"
 	local ipaddr
 	local netmask
-	local proto
 	
 	config_get ipaddr "$config" ipaddr
 	config_get netmask "$config" netmask
-	config_get proto "$config" proto
 	config_get gateway "$config" gateway
 
 	echo "Configuring local device with ip $ipaddr and netmask $netmask"	
 	ifconfig tap0 $ipaddr netmask $netmask up
 	route add default gw $gateway
 	
-	#assume dhcp for now...
+	# in-band control doesn't support dhcp for the local port
 	#udhcpc -i tap0 -b
 }
 	
